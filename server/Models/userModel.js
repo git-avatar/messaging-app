c14a07998f5b7286fffbe74131aad23a1d964b7f
avatar_ap <alek.pencic@gmail.com>
2024-01-30 21:06:31 +0100

    Adding chat logic and message logic.

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-    {
-        name: { type: String, requiered: true, minlength: 5, maxlength: 30 },
-        email: { type: String, requiered: true, minlength: 5, maxlength: 200, unique: true },
-        password: { type: String, requiered: true, minlength: 5, maxlength: 1024 }
-    },
-    {
-        timestamps: true
-    }
+  {
+    name: { type: String, requiered: true, minlength: 5, maxlength: 30 },
+    email: { type: String, requiered: true, minlength: 5, maxlength: 200, unique: true },
+    password: { type: String, requiered: true, minlength: 5, maxlength: 1024 }
+  },
+  {
+    timestamps: true
+  }
 );
 
 const userModel = mongoose.model("User", userSchema);
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoute = require("./Routes/userRoute");
+const chatRoute = require("./Routes/chatRoute");
+const messageRoute = require("./Routes/messageRoute");
 
 const app = express();
 require("dotenv").config();
@@ -9,19 +11,21 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 app.use("/api/users", userRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to our chat APIs..");
+  res.send("Welcome to our chat APIs..");
 });
 
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
 
 app.listen(port, (req, res) => {
-    console.log(`Server running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
 
 mongoose
-    .connect(uri)
-    .then(() => console.log("MongoDB connection established"))
-    .catch((error) => console.log("MongoDB connection failed: ", error.message));
+  .connect(uri)
+  .then(() => console.log("MongoDB connection established"))
+  .catch((error) => console.log("MongoDB connection failed: ", error.message));
