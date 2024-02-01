7f29533d940de1f2d455e13da32767e7b85248d3
avatar_ap <alek.pencic@gmail.com>
2024-02-01 17:57:47 +0100

    Adding initiate chat logic.

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useCallback } from "react";
 import { baseUrl, getRequest, postRequest } from "../utils/services";
 
 export const ChatContext = createContext();
@@ -7,6 +7,42 @@ export const ChatContextProvider = ({ children, user }) => {
   const [userChats, setUserChats] = useState(null);
   const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
   const [userChatsError, setUserChatsError] = useState(null);
+  const [potentialChats, setPotentialChats] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+
+      //if (!user || !user._id) {
+        // Ensure user and user._id are not null before proceeding
+      //  return;
+      //}
+      const response = await getRequest(`${baseUrl}/users`);
+      if (response.error) {
+        return console.log("Error fetching users!", response);
+      }
+
+      console.log("User1", user);
+
+      const pChats = response.filter((u) => {
+
+        console.log("UUUU", u);
+        let isChatCreated = false;
+        if (user?._id === u._id) return false;
+
+        if (userChats) {
+          isChatCreated = userChats?.some((chat) => {
+            return chat.members[0] === u._id || chat.members[1] === u._id;
+          })
+        }
+
+        return !isChatCreated;
+      });
+
+      setPotentialChats(pChats);
+    };
+
+    getUsers();
+  }, [userChats]);
 
   useEffect(() => {
     const getUserChats = async () => {
@@ -28,15 +64,29 @@ export const ChatContextProvider = ({ children, user }) => {
     getUserChats();
   }, [user]);
 
-  return <ChatContext.Provider
-    value={{
-      userChats,
-      isUserChatsLoading,
-      userChatsError
-    }}
-  >
-    {children}
-  </ChatContext.Provider>
+  const createChat = useCallback(async(firstId, secondId)=>{
+    const response = await postRequest(`${baseUrl}/chats`, JSON.stringify({firstId, secondId}));
+
+    if(response.error){
+      return console.log("Error creating chat!", response);
+    }
+
+    setUserChats((prev) => [...prev, response]);
+  }, [])
+
+  return (
+    <ChatContext.Provider
+      value={{
+        userChats,
+        isUserChatsLoading,
+        userChatsError,
+        potentialChats,
+        createChat
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
 }
 
 
