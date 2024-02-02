518046377a0a78765592be3d01371818d2c1744d
avatar_ap <alek.pencic@gmail.com>
2024-02-02 18:08:36 +0100

    Adding sockets.

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect, useCallback } from "react";
 import { baseUrl, getRequest, postRequest } from "../utils/services";
+import { io } from "socket.io-client";
 
 export const ChatContext = createContext();
 
@@ -14,16 +15,58 @@ export const ChatContextProvider = ({ children, user }) => {
   const [messagesError, setMessagesError] = useState(null);
   const [sendTextMessageError, setSendTextMessageError] = useState(null);
   const [newMessage, setNewMessage] = useState(null);
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
 
-  console.log("messages", messages)
+  console.log("onlineUsers", onlineUsers)
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:3000");
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (socket === null) return;
+    socket.emit("addNewUser", user?._id);
+    socket.on("getOnlineUsers", (res) => {
+      setOnlineUsers(res);
+    });
+
+    return () => {
+      socket.off("getOnlineUsers");
+    };
+  }, [socket]);
+
+  // send message
+  useEffect(() => {
+    if (socket === null) return;
+
+    const recipientId = currentChat?.members?.find((id) => id !== user?._id);
+
+    socket.emit("sendMessage", { ...newMessage, recipientId });
+  }, [newMessage]);
+
+  // recieve message
+  useEffect(() => {
+    if (socket === null) return;
+
+    socket.on("getMessage", res => {
+      if (currentChat?._id !== res.chatId) return;
+
+      setMessages((prev) => [...prev, res]);
+    });
+
+    return () => {
+      socket.off("getMessage");
+    }
+  }, [socket, currentChat]);
 
   useEffect(() => {
     const getUsers = async () => {
-
-      //if (!user || !user._id) {
-      // Ensure user and user._id are not null before proceeding
-      //  return;
-      //}
       const response = await getRequest(`${baseUrl}/users`);
       if (response.error) {
         return console.log("Error fetching users!", response);
@@ -138,12 +181,11 @@ export const ChatContextProvider = ({ children, user }) => {
         isMessagesLoading,
         messagesError,
         currentChat,
-        sendTextMessage
+        sendTextMessage,
+        onlineUsers
       }}
     >
       {children}
     </ChatContext.Provider>
   );
 }
-
-
